8a242d3bb6a61db7b4c615a3941150eefc5983b3
Raimundo Nonato de O Martins Jr <raimundo.n.martins@equatorialenergia.com.br>
2023-07-11 13:50:14 -0300

    init

--- servicos/application.js
+++ servicos/application.js
@@ -3,19 +3,19 @@ const axios = require("axios"); // node
 async function ProcessC6Bank() {
   console.log("Iniciando Processo de Captura C6 Bank");
   axios
-    .get("http://localhost:3001/api/app")
+    .get("http://149.100.154.155:3001/api/app")
     .then((resp) => {
       console.log("Iniciando Processo de Criação C6 Bank");
       axios
-        .get("http://localhost:3001/api/create-user/c6bank")
+        .get("http://149.100.154.155:3001/api/create-user/c6bank")
         .then(() => {
           console.log("Iniciando Processo de Reset C6 Bank");
           axios
-            .get("http://localhost:3001/api/reset-user/c6bank")
+            .get("http://149.100.154.155:3001/api/reset-user/c6bank")
             .then(() => {
               console.log("Iniciando Processo de Atualização C6 Bank");
               axios
-                .get("http://localhost:3001/api/update-2tech")
+                .get("http://149.100.154.155:3001/api/update-2tech")
                 .then(() => {
                   console.log(
                     "Processo de Criação e Reinicialização Finalizado"
