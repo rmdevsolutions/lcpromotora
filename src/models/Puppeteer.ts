8b0472d76d815b426e60a0af415307724ac360a9
Raimundo Nonato de O Martins Jr <raimundo.n.martins@equatorialenergia.com.br>
2023-07-12 20:35:51 -0300

    init

--- src/models/Puppeteer.ts
+++ src/models/Puppeteer.ts
@@ -22,7 +22,7 @@ class Puppeteer {
     this.browser = await puppeteer.launch({
       args: ["--no-sandbox"],
       protocolTimeout: 0,
-      headless: false,
+      headless: "new",
     });
     this.page = await this.browser.newPage();
     await this.page.setViewport({ width: 1920, height: 1080 });
